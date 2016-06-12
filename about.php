2c59d611e3d16481ddbffdf63f093429d024fbae
suparpat <reminiz@gmail.com>
2016-06-12 21:50:03 +0700

    Make comment box width 100%. Edit texts about and how to pages.

--- about.php
+++ about.php
@@ -17,33 +17,15 @@
 				<h3>About</h3>
 			</header>
 			<!--To do: make this page with ckeditor?-->
-			<h4>Learny is place to share knowledge—</h4>
-			<p style="text-indent: 50px;">
-				A place where curious learners can enjoy contents contributed by others. The
-				content allowed here is not limited to encyclopedialike content, but also
-				insights, thoughts, and ideas.
-			</p>
-			<p style="text-indent: 50px;">
-				As such, learners must take into consideration that contents published here are
-				by no means guaranteed to be factual, and a certain amount of critical thinking
-				should be applied when reading.
-			</p>
-
-			<p style="text-indent: 50px;">
-				Furthermore, discussions are highly encouraged and comments should be the main
-				method of doing so. This should allow facts to be cross-checked, thoughts discussed,
-				constructive criticisms made, and ideas validated.
-			</p>
-
-			<h4>Inspiration</h4>
-			<p style="text-indent: 50px;">
-				Learny is inspired by the concept of being a community driven place. With the like of
-				Wikipedia, Reddit, Medium, 9Gag, Metafilter, LibriVox, etc.
+			<h4>Learny is place to share knowledge, stories, thoughts, ideas, et cetera—</h4>
+			<p style="text-indent: 0px;">
+				Learny wants to be the place where people come to learn and share. Some of Learny's inspirations include:
+				Reddit (the upvote/downvote feature and design), StackOverflow (the reputation system) and Wikipedia.
 			</p>
 
 			<h4>Design</h4>
-			<p style="text-indent: 50px;">
-				Learny's design is inspired by
+			<p style="text-indent: 0px;">
+				Learny has a clutter-free design, allowing its users to focus on the actual contents. The design is actually based on <a href="https://pinboard.in/">Pinboard</a>'s design combined with those of other places.
 			</p>
 			<?php include 'partials/quote_block.php' ?>
 			<?php include 'partials/footer.php' ?>
--- how-to.php
+++ how-to.php
@@ -16,11 +16,11 @@
 			<header>
 				<h3>How to</h3>
 			</header>
-			<aside>Share what you know!</aside>
+			<aside>Use Learny to:</aside>
 			<ol>
+				<li>Read and learn from what others have shared</li>
 				<li>Share facts, ideas, thoughts, stories, etc.</li>
-				<li>Make notes</li>
-				<li>Public or private, up to you</li>
+				<li>Make and keep notes in the cloud</li>
 			</ol>
 			<?php include 'partials/quote_block.php' ?>
 			<?php include 'partials/footer.php' ?>
