09797988b74b4c5022aebd30a2f03939b19c787e
suparpat <reminiz@gmail.com>
2016-06-15 00:30:17 +0700

    Add icon for apple devices

--- partials/header.php
+++ partials/header.php
@@ -3,3 +3,4 @@
 <!-- http://www.sitepoint.com/10-ways-make-website-mobile-friendly/ -->
 <meta name=viewport content='width=482'>
 <link rel="icon" type="image/png" href="img/logo.png"/>
+<link rel="apple-touch-icon" href="img/logo.png"/>
